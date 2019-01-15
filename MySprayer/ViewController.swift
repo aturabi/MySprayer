810f6dd122bf782ce36f56365732c291e7e72f73
Areeba Turabi <aturabi@ucdavis.edu>
2019-01-14 20:27:42 -0800

    Working navigation and basic untested camera functions

--- MySprayer/ViewController.swift
+++ MySprayer/ViewController.swift
@@ -10,10 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ImageDisplay: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    // Navigation Buttons
+    @IBAction func ToCamPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "MainToCam", sender: self)
+    }
+    
+    @IBAction func ToPredictPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "MainToPredict", sender: self)
+    }
+    
+    @IBAction func ToAboutPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "MainToAbout", sender: self)
+    }
+    
+    @IBAction func AboutToMainPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "AboutToMain", sender: self)
+    }
+    
+    @IBAction func PredictToMainPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "PredictToMain", sender: self)
+    }
+    
+    // camera functions
+    @IBAction func TakePhoto(_ sender: UIButton) {
+        let picker = UIImagePickerController()
+        
+        picker.sourceType = .camera
+        
+        present(picker, animated:true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+    didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        ImageDisplay.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage;
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
 
